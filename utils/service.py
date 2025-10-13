68a7dc0bcad7ca06bd69c871f4945dcc736607b6
Sofía Fernandez <soffia.bf@gmail.com>
2025-10-12 21:21:50 -0300

    Fix encabezado

--- utils/service.py
+++ utils/service.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 def get_data(filename):
     data_csv = []
@@ -9,7 +10,21 @@ def get_data(filename):
     return data_csv
 
 def set_data(filename, data):
+    # Preservar la primera línea (encabezado) del CSV si existe.
+    header_line = None
+    if os.path.exists(filename):
+        try:
+            with open(filename, mode='r', encoding='UTF-8', newline='') as f:
+                first = f.readline()
+                if first:
+                    header_line = first.rstrip('\n')
+        except Exception:
+            header_line = None
+
     with open(filename, mode='w', encoding='UTF-8', newline='') as file:
+        # Si había una cabecera original, la reescribimos tal cual (preservando formato)
+        if header_line:
+            file.write(header_line + "\n")
         writer = csv.writer(file)
         writer.writerows(data)
 
